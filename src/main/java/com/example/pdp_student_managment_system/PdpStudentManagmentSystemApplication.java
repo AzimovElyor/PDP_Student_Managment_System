e85d22d82a9cef307a23e881c20b2655b70e6361
Elyor Azimov <elyorazimov098@gmail.com>
2023-10-05 10:38:30 +0500

    Hello

--- src/main/java/com/example/pdp_student_managment_system/PdpStudentManagmentSystemApplication.java
+++ src/main/java/com/example/pdp_student_managment_system/PdpStudentManagmentSystemApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PdpStudentManagmentSystemApplication {
 
     public static void main(String[] args) {
+        System.out.println("EHello");
         SpringApplication.run(PdpStudentManagmentSystemApplication.class, args);
     }
 
