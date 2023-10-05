d18dfa0d71830c7baf47109a7c988e41b85927a9
Elyor Azimov <elyorazimov098@gmail.com>
2023-10-05 10:32:14 +0500

    Project init

---
+++ src/main/java/com/example/pdp_student_managment_system/PdpStudentManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.pdp_student_managment_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdpStudentManagmentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdpStudentManagmentSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pdp_student_managment_system/PdpStudentManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pdp_student_managment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdpStudentManagmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
